2a8464692fb27b4dd1443e57da1fc566beb0d47b
lind.zhang <bfyxzls@sina.com>
2018-05-14 18:23:29 +0800

    add server config

---
+++ src/main/java/lind/springdemo/config/clientshop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package lind.springdemo.config.clientshop.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  @Value("${server.port}") // git配置文件里的key
+      String myww;
+
+  @RequestMapping("/")
+  public String index() {
+    return "myww=" + myww;
+  }
+}
